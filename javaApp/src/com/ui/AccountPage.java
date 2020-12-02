bb1266991415fe322453ea9774d252a9f95def91
tucansw <no-reply@no-reply.com>
2020-12-02 21:29:11 +0100

    Combine Home prototype into AccountPage
    The functionality contained in the Home view prototype
    has been completely merged into the new AccountPage.

--- javaApp/src/com/ui/AccountPage.java
+++ javaApp/src/com/ui/AccountPage.java
@@ -2,12 +2,15 @@ package com.ui;
 
 import javax.swing.*;
 
+import com.User;
+
 class AccountPage extends Page {
     
     AccountPage(final Session session) {
         super(session);
 
-        add(new JLabel("Unfortunately, all your money has been lost. :("));
+        // TODO Expand account view
+        add(new JLabel("Welcome, " + User.getUsername() "! Balance: " + User.getBalance()));
     }
 
 }
