84ea84f8fe6297dae905a419376f3961eaccbbe2
tucansw <no-reply@no-reply.com>
2020-12-02 21:24:46 +0100

    Fix LookAndFeel code
    The look and feel was initialised at the wrong time,
    resulting in it not being set correctly.
    It is now initialised correctly.

--- javaApp/src/com/ui/MainFrame.java
+++ javaApp/src/com/ui/MainFrame.java
@@ -12,21 +12,22 @@ public class MainFrame extends JFrame {
     private Session session = new Session();
 
     public MainFrame() {
-        // Set the look and feel
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (Exception e) {
-            System.err.println("Could not set look and feel:");
-            e.printStackTrace();
-        }
-
         this.setLayout(new BorderLayout());
 
         // Add the session page container (wrapped in a scroll pane)
         this.add(new JScrollPane(session.pageContainer), BorderLayout.CENTER);
 
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        // Set the system look and feel
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            SwingUtilities.updateComponentTreeUI(this);
+        }
+        catch (Exception e) {
+            System.err.println("Could not set look and feel:");
+            e.printStackTrace();
+        }
     }
 
 }
